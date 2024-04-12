8841671d2a17118871d13a4b12e69fd7c1327961
SteveIsGlitched <96364634+SteveIsGlitched@users.noreply.github.com>
2024-04-12 17:24:50 +1000

    Add files via upload

---
+++ i3/fish/functions/fish_greeting.fish
@@ -0,0 +1,4 @@
+function fish_greeting
+echo ""
+neofetch
+end
---
+++ nvim/lua/polish.lua
@@ -0,0 +1,30 @@
+--if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- This will run last in the setup process and is a good place to configure
+-- things like custom filetypes. This just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+
+vim.api.nvim_create_augroup("neotree_autoopen", { clear = true })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  desc = "Open neo-tree on enter",
+  group = "neotree_autoopen",
+  callback = function()
+    if not vim.g.neotree_opened then
+      vim.cmd "Neotree show"
+      vim.g.neotree_opened = true
+    end
+  end,
+})
+
+-- Set up custom filetypes
+--[[vim.filetype.add {
+  extension = {
+    foo = "fooscript",
+  },
+  filename = {
+    ["Foofile"] = "fooscript",
+  },
+  pattern = {
+    ["~/%.config/foo/.*"] = "fooscript",
+  },
+}]]
